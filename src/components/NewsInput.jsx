419857aa912f12bb275e8ec1fd975e6e3b2c86ff
Danish Qumar <danishq7080@gmail.com>
2025-11-03 22:45:44 +0530

    url

--- src/components/NewsInput.jsx
+++ src/components/NewsInput.jsx
@@ -19,7 +19,7 @@ export default function NewsInput() {
     setResult(null);
 
     try {
-      const res = await fetch("https://rvmhp7-5000.csb.app/predict", {
+      const res = await fetch("https://lyjclq-5000.csb.app/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ title, text }),
